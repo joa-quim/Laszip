a524782c70c6aeea6f94f2690c15d5bbb420eafd
Joaquim Luis <jmfluis@gmail.com>
2015-12-16 00:01:17 +0000

    Export two more functions

--- src/Laszip.jl
+++ src/Laszip.jl
@@ -30,7 +30,7 @@ export
 	laszip_read_point,
 	laszip_close_reader,
 # Non C-wrapper functions
-	xyz2laz, laz2xyz, msgerror, las2dat, dat2las
+	xyz2laz, laz2xyz, msgerror, las2dat, dat2las, create_header, create_point
 
 include("laszip_h.jl")
 include("laszip_dll.jl")
@@ -60,6 +60,40 @@ end
 function msgerror(lzobj::Ptr{Ptr{Void}}, extramsg::AbstractString="")
 	lzobj = unsafe_load(lzobj)
 	msgerror(lzobj, extramsg)
+end
+
+# ---------------------------------------------------------------------------------------------
+function create_header(project_ID_GUID_data_4="", system_identifier="", generating_software="")
+# Initialize an empty (or almost) header type
+	if (isempty(project_ID_GUID_data_4))
+		project_ID_GUID_data_4 = Laszip.Array_8_UInt8(fill(Base.zero(UInt8),8)...)
+	end
+	if (isempty(system_identifier))
+		system_identifier = Laszip.Array_32_UInt8(fill(Base.zero(UInt8),32)...)
+	end
+	if (isempty(generating_software))
+		generating_software = Laszip.Array_32_UInt8(fill(Base.zero(UInt8),32)...)
+	end
+	number_of_points_by_return = Laszip.Array_5_UInt32(fill(Base.zero(UInt32),5)...)
+	extended_number_of_points_by_return = Laszip.Array_15_Culonglong(fill(Base.zero(Culonglong),15)...)
+	user_data_in_header = pointer("")
+	vlrs = pointer([laszip_vlr])
+	user_data_after_header = pointer("")
+	Laszip.laszip_header(0, 0, 0, 0, 0, project_ID_GUID_data_4, '1', '2', system_identifier, generating_software,
+	                     0, 0, 0, 0, 0, '0', 0, 0, number_of_points_by_return, 1., 1., 1., 0., 0., 0.,
+	                     0., 0., 0., 0., 0., 0., 0, 0, 0, 0, extended_number_of_points_by_return,
+	                     0, user_data_in_header, vlrs, 0, user_data_after_header)
+end
+
+
+# ---------------------------------------------------------------------------------------------
+function create_point()
+# Initialize an empty point type
+	rgb = Laszip.Array_4_UInt16(fill(Base.zero(UInt16),4)...)
+	dummy = Laszip.Array_7_Cuchar(fill(Base.zero(Cuchar),7)...)
+	wave_packet = Laszip.Array_29_Cuchar(fill(Base.zero(Cuchar),29)...)
+	laszip_point(0, 0, 0, 0, '0', '0', '0', '0', '0', '0', '0', '0', 0, '0', 0, 0, '0', '0', '0', '0', '0', '0',
+	             dummy, 0., rgb, wave_packet, 0, pointer(""))
 end
 
 end # module
